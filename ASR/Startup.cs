5e75f7f9c09f03b49de5e2214336a0beb322142f
Daniel Milner <danielmmilner@gmail.com>
2019-01-31 12:24:42 +1100

    Improved site layout

--- ASR/Startup.cs
+++ ASR/Startup.cs
@@ -7,7 +7,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using ASR.Models;
-using System.Globalization;
 using Microsoft.AspNetCore.Identity;
 
 namespace ASR
@@ -77,7 +76,6 @@ namespace ASR
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-            //app.UseCookiePolicy();
             app.UseAuthentication();
 
             app.UseMvc(routes =>
