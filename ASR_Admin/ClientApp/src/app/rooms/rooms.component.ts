3cc7e58ea8773c7aa1bd1b47257782b7d6086a65
Tyler Watkins <tjwato@gmail.com>
2019-02-02 21:21:43 +1100

    Add room edit

--- ASR_Admin/ClientApp/src/app/rooms/rooms.component.ts
+++ ASR_Admin/ClientApp/src/app/rooms/rooms.component.ts
@@ -10,10 +10,20 @@ export class RoomsComponent {
   public rooms: Room[];
 
 
-  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<Room[]>(baseUrl + 'api/slot/GetAllSlots').subscribe(result => {
+  constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) {
+    http.get<Room[]>(baseUrl + 'api/room/').subscribe(result => {
       this.rooms = result;
     }, error => console.error(error));
+  }
+
+
+
+  editRoomName(id: string) {
+    //let r = this.rooms.find(x => x.roomId == id);
+    //r.roomName = document.getElementById("roomName-" + id).;
+    //this.http.put<void>(this.baseUrl + 'api/room/' + id, r).subscribe(result => {
+      
+    //}, error => console.error(error));
   }
 
 }
---
+++ ASR_Admin/Models/RoomDataAccessLayer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASR_Admin.Models
+{
+    public class RoomDataAccessLayer
+    {
+        private readonly dbContext db = new dbContext();
+
+        public IEnumerable<ApiRoomModel> GetAllRooms()
+        {
+            return db.Room.Select(x => new ApiRoomModel {
+                RoomId = x.RoomId,
+                RoomName = x.RoomName
+            });
+        }
+
+        public ApiRoomModel GetRoom(string id)
+        {
+            return GetAllRooms().FirstOrDefault(x => x.RoomId == id);
+        }
+
+        public void UpdateRoomName(string roomId, string roomName)
+        {
+            var room = db.Room.FirstOrDefault(x => x.RoomId == roomId);
+            room.RoomName = roomName;
+
+            db.Room.Update(room);
+
+            db.SaveChanges();
+
+        }
+    }
+}
